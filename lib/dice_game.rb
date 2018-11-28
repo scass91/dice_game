12d4be550c3ecc1f476a235575b375ebab086c4d
Simon Cass <scass1991@gmail.com>
2018-11-28 12:39:13 +0000

    Add ability to roll more die, removed test for value between 1&6

--- lib/dice_game.rb
+++ lib/dice_game.rb
@@ -1,5 +1,15 @@
 class Dice
-  def roll
-    1 + rand(6)
+  def roll(number)
+    roll_array = []
+    number.times do
+      roll_value = rand(1..6)
+      roll_array << roll_value
+    end
+    total = 0
+    roll_array.each do |roll|
+      new_total = total + roll
+      total = new_total
+    end
+    total
   end
 end
--- spec/dice_game_spec.rb
+++ spec/dice_game_spec.rb
@@ -2,7 +2,10 @@ require 'dice_game'
 
 RSpec.describe Dice do
   it { is_expected.to respond_to(:roll) }
-  it "returns a number between 1 & 6" do
-    expect(Dice.new.roll).to be_between(1, 6).inclusive
+#  it "returns a number between 1 & 6" do
+#    expect(Dice.new.roll).to be_between(1, 6).inclusive
+#  end
+  it "Allows a user to roll any number of dice" do
+      expect(Dice.new.roll(4)).to be_between(4, 24).inclusive
   end
 end
